235d4c616fd85e5be5d8f2a6f43212a751d9e4d4
Yasheed1995 <qwerty8608301@gmail.com>
2021-10-19 09:22:45 +0800

    finish autoBuy 2 & 3

---
+++ run.sh
@@ -0,0 +1,3 @@
+# usage: python3 idx real_buy
+python3 test.py 2 0
+python3 test.py 3 0
--- test.py
+++ test.py
@@ -7,25 +7,63 @@ import time
 from selenium.common.exceptions import NoSuchElementException as NoSuchElementException
 from selenium.common.exceptions import ElementNotInteractableException as ElementNotInteractableException
 import threading
-
+import sys
 # define driver
-chromedriver = '/opt/homebrew/bin/chromedriver'
+chromedriver = '/usr/local/bin/chromedriver'
+#chromedriver = '/opt/homebrew/bin/chromedriver'
 driver = webdriver.Chrome(chromedriver)
 cart_url = "https://store.isseymiyake.com/p/cart?type=purchase"
-url = 'https://store.isseymiyake.com/c/ha_all_all/HA15JT111'
 
-item_to_buy = {
-	(0,0):1,
-	(1,0):1
-}
+options = [
+	{
+		'url': 'https://store.isseymiyake.com/c/ha_all_all/HA15JT111', 
+		'item_to_buy': {
+			(0,0): 1,
+			(1,0): 1
+		},
+		'account':'www777.hung@gmail.com',
+		'password':'777seven',
+		'description': 'testing'
+	},
+	{
+		'url': 'https://store.isseymiyake.com/c/la_all_all/LA13FC185',
+		'item_to_buy': {
+			(0, 1): 1,
+			(1, 1): 2
+		},
+		'account':'qwerty8608301@gmail.com',
+		'password':'9FMp3.!.2mE6e6u',
+		'description': 'testing'
+	},
+	{
+		'url': 'https://store.isseymiyake.com/c/pl_all_all/PP13JH157',
+		'item_to_buy': {
+			(0, 1): 4,
+			(0, 2): 4,
+			(0, 3): 2
+		},
+		'account': 'www111.hung@gmail.com',
+		'password': '777seven',
+		'description': ''
+	},
+	{
+		'url': 'https://store.isseymiyake.com/c/pl_all_all/PP13JC152',
+		'item_to_buy': {
+			(0, 1): 2
+		},
+		'account': 'www222.hung@gmail.com',
+		'password': '777seven',
+		'description': ''
+	}
+]
 
 #login
-def login(driver):
+def login(account, password):
 	start_time = time.time()
 	print("try to login!")
 	driver.get('https://store.isseymiyake.com/my/top')
-	driver.find_element_by_name('mailAddress').send_keys("www777.hung@gmail.com")
-	driver.find_element_by_name('password').send_keys("777seven")
+	driver.find_element_by_name('mailAddress').send_keys(account)
+	driver.find_element_by_name('password').send_keys(password)
 	print("click login button!")
 	try:
 		loginButton = driver.find_elements_by_class_name("fs-c-button--login")
@@ -34,9 +72,8 @@ def login(driver):
 		pass
 		
 	print('---%s seconds ---' % (time.time() - start_time))
-
 	
-def check_items_quantity(driver, url):
+def check_items_quantity(item_to_buy, url):
 	start_time = time.time()
 	driver.get(url)
 	print("item name you're checking is: ", driver.title.split('|')[0])
@@ -61,12 +98,12 @@ def check_items_quantity(driver, url):
 		print('---%s seconds ---' % (time.time() - start_time))
 		return item_cnt
 	except NoSuchElementException:
-		print("no product name!")
+		print("No product list!")
 		
 	print('---%s seconds ---' % (time.time() - start_time))
 	return 0
 	
-def finish_buy(driver, confirm):
+def finish_buy(confirm):
 	start_time = time.time()
 	finish_flag = False
 	print("go to cart!")
@@ -80,15 +117,18 @@ def finish_buy(driver, confirm):
 			print('finish!')
 			finish_flag = True
 		except IndexError:
+			cnt = 0
 			print('no purchase button! close toast!')
 			closeButton = driver.find_elements_by_class_name('iziToast-close')
 			try:
 				for i in closeButton:
 					i.click()
+					cnt += 1
 					
 			except selenium.common.exceptions.StaleElementReferenceException:
 				break
-				pass
+			if cnt > 10:
+				return False
 			
 	try:
 		Cash_on_delivery_button = driver.find_element_by_xpath(("//label[@for='fs_input_payment_cashOnDelivery']"));
@@ -103,7 +143,7 @@ def finish_buy(driver, confirm):
 		confirmButton[0].click()
 	print('---%s seconds ---' % (time.time() - start_time))
 	
-def clear_cart(driver):
+def clear_cart():
 	start_time = time.time()
 	print("go to cart!")
 	driver.get(cart_url)
@@ -125,27 +165,38 @@ def clear_cart(driver):
 			clear_flag = True
 	print('---%s seconds ---' % (time.time() - start_time))
 	
-def main(driver, url=url,restart=False):	
+def main(idx=0, restart=False):	
+	url = options[idx]['url']
+	item_to_buy = options[idx]['item_to_buy']
+	account = options[idx]['account']
+	password = options[idx]['password']
 	if not restart:
-		login(driver)
-		clear_cart(driver)
-		#clear_cart()
-		
-	item_quantity = check_items_quantity(driver, url)
-	print("quantity: ", item_quantity)
-	while item_quantity == 0:
-		print("No product to buy! sleep 10 secs")
-		time.sleep(5)
-		clear_cart(driver)
-		driver.refresh()
-		item_quantity = check_items_quantity(driver, url)
+		login(account=account, password=password)
+		clear_cart()
+
+	item_need_to_buy_count = sum(item_to_buy.values())
+
+	while item_need_to_buy_count > 0:
+		item_need_to_buy_count = sum(item_to_buy.values())
+		print("item need to buy count: ", item_need_to_buy_count)
+		print("item_to_buy: ", item_to_buy)
+		item_quantity = check_items_quantity(item_to_buy, url)
 		print("quantity: ", item_quantity)
-		
-	if item_quantity:
-		finish_buy(driver, False)  
+		while item_quantity == 0:
+			print("No product to buy! sleep 10 secs")
+			time.sleep(5)
+			driver.refresh()
+			item_quantity = check_items_quantity(item_to_buy, url)
+			print("quantity: ", item_quantity)
+			
+		if item_quantity:
+			finish_buy(False) 
+			clear_cart() 
 			
 if __name__ == '__main__':
-	main(url=url,driver=driver, restart=False)
+	idx = int(sys.argv[1])
+	print('idx is: ', idx)
+	main(idx=idx, restart=False)
 	while True:
 		pass
 	
