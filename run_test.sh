c428cb93660179b3638e791cf30b3c1142504bd6
Yasheed1995 <qwerty8608301@gmail.com>
2021-10-24 21:06:15 +0800

    add terminate.sh

--- run_test.sh
+++ run_test.sh
@@ -1,5 +1,6 @@
 # usage: python3 idx real_buy
-python3 test.py 0 0 2 >> err0.log 1 >> out0.log &
-python3 test.py 1 0 2 >> err1.log 1 >> out1.log &
-python3 test.py 2 0 2 >> err2.log 1 >> out2.log &
-python3 test.py 3 0 2 >> err3.log 1 >> out3.log &
+for i in {0..2}
+do
+    echo "running $i item!"
+    python3 test.py $i 0 &
+done
