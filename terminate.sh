b3a58b4dd03ffc0ea59dec2f50f1a1c55afc86b2
Yasheed1995 <qwerty8608301@gmail.com>
2022-02-03 12:49:09 +0800

    modify terminate.sh

--- terminate.sh
+++ terminate.sh
@@ -1,6 +1,6 @@
 echo "kill all chrome!"
 pid=$(ps -fe | grep 'chrome' | grep -v grep | awk '{print $2}')
-echo "process running: "
-echo $pid
+#echo "process running: "
+#echo $pid
 pkill -9 "Google Chrome"
 echo "terminated!"
