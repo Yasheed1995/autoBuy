c7867bbba2b1ab4571c2c8d6bbd6832a84206688
陳鴻智 <qwerty8608301@gmail.com>
2022-11-03 15:50:41 +0800

    add thorium terminate

--- terminate.sh
+++ terminate.sh
@@ -2,5 +2,5 @@ echo "kill all chrome!"
 pid=$(ps -fe | grep 'chrome' | grep -v grep | awk '{print $2}')
 #echo "process running: "
 #echo $pid
-pkill -9 "Google Chrome"
+pkill -9 "Thorium"
 echo "terminated!"
